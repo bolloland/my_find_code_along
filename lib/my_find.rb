bddf823d0174973ab62d21ad265f8d68b130286c
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-11-18 17:08:38 -0600

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,4 +2,9 @@ require 'pry'
 
 def my_find(collection)
 
+    i = 0
+    while i < collection.length
+             return collection[i] if yield(collection[i])  ##return the first true element in the array (15)
+        i  += 1
+    end
 end
